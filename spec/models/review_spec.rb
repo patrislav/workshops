66bb718f97478c17c06f37ebb7b44016e359fc21
Patryk Kalinowski <xivrox@gmail.com>
2014-11-26 12:16:50 +0100

    Finish Review model

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,6 +6,6 @@ describe Review do
     it { is_expected.to validate_presence_of :rating }
     it { is_expected.to validate_presence_of :user_id }
     it { is_expected.to belong_to :user }
-
+    it { is_expected.to belong_to :product }
   end
 end
