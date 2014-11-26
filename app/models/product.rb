87cfee36d0aa9ceb3201c44c832eb226fe37c831
Patryk Kalinowski <xivrox@gmail.com>
2014-11-26 12:11:42 +0100

    Finish Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,22 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title, :description, :price
+  validates_numericality_of :price
+  validate :price_decimal_places_limit
+
+  def average_rating
+    @average_rating ||= reviews.average(:rating)
+  end
+
+
+  private
+    def price_decimal_places_limit
+      parts = price.to_s.split('.')
+      if parts.last and parts.last.size > 2
+        errors.add(:price, 'Price should have no more than 2 decimal places.')
+      end
+    end
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,28 +5,32 @@ describe Product do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :description }
     it { is_expected.to validate_presence_of :price }
+    it { is_expected.to validate_numericality_of :price }
+    it { is_expected.to belong_to :category }
+    it { is_expected.to belong_to :user }
+    it { is_expected.to have_many :reviews }
+  end
 
-    describe '#price' do
-      let(:product) { build(:product, price: 1.234) }
+  describe '#price' do
+    let(:product) { build(:product, price: 1.234) }
 
-      it 'is limited to two decimal places' do
-        expect(product).to_not be_valid
-      end
+    it 'is limited to two decimal places' do
+      expect(product).to_not be_valid
+    end
+  end
+
+  describe '#average_rating' do
+    let(:user)    { create(:user) }
+    let(:product) { create(:product) }
+    let(:review1) { create(:review, rating: 2, user: user) }
+    let(:review2) { create(:review, rating: 3, user: user) }
+
+    before do
+      product.reviews << [review1, review2]
     end
 
-    describe '#average_rating' do
-      let(:user)    { create(:user) }
-      let(:product) { create(:product) }
-      let(:review1) { create(:review, rating: 2, user: user) }
-      let(:review2) { create(:review, rating: 3, user: user) }
-
-      before do
-        product.reviews << [review1, review2]
-      end
-
-      it 'calculates average rating' do
-        expect(product.average_rating).to eq 2.5
-      end
+    it 'calculates average rating' do
+      expect(product.average_rating).to eq 2.5
     end
   end
 end
