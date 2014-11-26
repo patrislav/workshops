1d9001343ecd92d07cada8b6955bba8789669cf2
Patryk Kalinowski <xivrox@gmail.com>
2014-11-26 12:13:30 +0100

    Finish Category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Category do
-  it { is_expected.to validate_uniqueness_of(:name) }
+  describe 'validations' do
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to have_many :products }
+  end
 end
