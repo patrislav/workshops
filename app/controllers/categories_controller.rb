c6298d7afdb0d3758f9b6d890273cf652b4ccc0d
Patryk Kalinowski <xivrox@gmail.com>
2014-11-26 11:23:47 +0100

    Authorize for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
+  before_action :authorize, only: [:new, :edit, :update, :destroy, :create]
   
   expose(:categories)
   expose(:category)
@@ -43,5 +44,9 @@ class CategoriesController < ApplicationController
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+
+    def authorize
+      redirect_to new_user_session_path if not current_user.admin?
     end
 end
