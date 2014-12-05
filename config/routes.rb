a479871af114b580605d8e3754a56c6a40b30dff
Patryk Kalinowski <xivrox@gmail.com>
2014-12-05 13:13:41 +0100

    Start design

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :categories do
-    resources :products do
+    resources :products, except: [:index] do
       resources :reviews
     end
   end
