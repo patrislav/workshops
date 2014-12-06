3574fe6cf1c7f51a02dfcfdc8d6c050efa2d9693
Patryk Kalinowski <xivrox@gmail.com>
2014-12-06 19:14:54 +0100

    Configure mailer and add Procfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,18 @@ Workshops::Application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['GMAIL_USERNAME'],
+    :password       => ENV['GMAIL_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => ENV['BASE_URI'] || 'http://localhost' }
+
 end
