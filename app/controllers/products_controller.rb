95f025fa05da736dd297e70b8b16056a9379417b
Patryk Kalinowski <xivrox@gmail.com>
2014-12-06 16:56:51 +0100

    Fix bug not saving user with the product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,7 @@ class ProductsController < ApplicationController
 
   def create
     self.product = Product.new(product_params)
+    self.product.user = current_user
 
     if product.save
       category.products << product
